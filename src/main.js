a6599b6083c660828b4d99ad539d585b4dc6a6c2
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-07-17 16:58:21 -0700

    Close modal when users click on outside the modal

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const modalButton = document.querySelector('.jsModalButton')
 const modalCloseBtn = document.querySelector('.jsModalClose')
 const body = document.body
+const modalOverlay = document.querySelector('.modal-overlay')
 
 modalButton.addEventListener('click', e => {
     // console.log('button clicked!')
@@ -9,4 +10,12 @@ modalButton.addEventListener('click', e => {
 
 modalCloseBtn.addEventListener('click', e => {
     body.classList.remove('modal-is-open')
+})
+
+// add an event listener to the overlay
+modalOverlay.addEventListener('click', e => {
+    if (!e.target.closest('.modal')) {
+        //close modal
+        body.classList.remove('modal-is-open')
+    }
 })
