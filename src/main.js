6e021f151b08cb5b33a7bd41e48c8f07e59c6054
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-07-22 16:31:44 -0700

    Animate the waving hand

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
 const modalButton = document.querySelector('.jsModalButton')
 const modalCloseBtn = document.querySelector('.jsModalClose')
-const body = document.body
 const modalOverlay = document.querySelector('.modal-overlay')
 
 modalButton.addEventListener('click', e => {
-    // console.log('button clicked!')
-    body.classList.add('modal-is-open')
+    document.body.classList.add('modal-is-open')
 })
 
 modalCloseBtn.addEventListener('click', e => {
-    body.classList.remove('modal-is-open')
+    document.body.classList.remove('modal-is-open')
 })
 
 // add an event listener to the overlay
 modalOverlay.addEventListener('click', e => {
     if (!e.target.closest('.modal')) {
         //close modal
-        body.classList.remove('modal-is-open')
+        document.body.classList.remove('modal-is-open')
     }
 })
