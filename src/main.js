77eb3d7fbcaa133350e308ceee40e26dd6b40585
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-07-25 15:09:06 -0700

    Refactor animations to use GSAP instead of CSS keyframes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,45 @@
+import { gsap } from "gsap";
+
 const modalButton = document.querySelector('.jsModalButton')
 const modalCloseBtn = document.querySelector('.jsModalClose')
 const modalOverlay = document.querySelector('.modal-overlay')
+const wavingHand = document.querySelector('.wave-hand')
 
+
+// Point hand animation
+window.addEventListener('DOMContentLoaded', () => {
+    const pointHand = document.querySelector('.point-hand')
+    
+    gsap.from(pointHand, {
+        y: '4em',
+        opacity: 0,
+        duration: 1,
+        // ease: 'power2.out', commented out 
+        ease: 'cubic-bezier(0.18,0.89,0.32,1.28)',
+    })
+})
+
+// Wave hand animation
+function wave() {
+    const t1 = gsap.timeline({})
+    t1.set(wavingHand, { transformOrigin: 'bottom center'})
+    t1.from(wavingHand, {
+        duration: 0.5,
+        scale: 0.25,
+        opacity: 0,
+        ease: 'back.out(1.5)',
+    })
+    t1.to(wavingHand, { duration: 0.2, rotation: 15,})
+    t1.to(wavingHand, { duration: 0.2, rotation: -15,})
+    t1.to(wavingHand, { duration: 0.2, rotation: 15,})
+    t1.to(wavingHand, { duration: 0.2, rotation: -15,})
+    t1.to(wavingHand, { duration: 0.2, rotation: 0,})
+}
+
+// Opens modal
 modalButton.addEventListener('click', e => {
     document.body.classList.add('modal-is-open')
+    wave()
 })
 
 modalCloseBtn.addEventListener('click', e => {
